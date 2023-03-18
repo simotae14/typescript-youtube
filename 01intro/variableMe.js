b2110526b25d64d337e8cd0e9fc4800d84a75a49
Simone Taeggi <startae14@gmail.com>
2023-03-18 12:59:41 +0100

    03_any-type

--- 01intro/variableMe.js
+++ 01intro/variableMe.js
@@ -1,2 +1,10 @@
+"use strict";
+exports.__esModule = true;
 var greetings = "Hello Simone";
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 334455.3; // with type inference
+// userId = 'simone'
+// boolean
+var isLoggedIn = false;
--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -1,9 +1,27 @@
 let greetings: string = "Hello Simone";
 
-let myNum = 6;
-
 greetings.toLowerCase();
 
 console.log(greetings);
+
+// number
+
+let userId = 334455.3; // with type inference
+// userId = 'simone'
+
+// boolean
+
+let isLoggedIn: boolean = false;
+
+// any
+
+let hero: string;
+
+function getHero() {
+  return 'thor';
+}
+
+hero = getHero();
+
 
 export {};
