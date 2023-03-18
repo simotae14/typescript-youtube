d30dfdc3184195c57f1a52a110c9ba6a96f68813
Simone Taeggi <startae14@gmail.com>
2023-03-18 17:29:52 +0100

    04_function_arguments_type

---
+++ 02Basics/myfunctions.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+    //return num + 2;
+    return "hello";
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+let myVal = addTwo(5);
+getUpper("simone");
+signUpUser("simone", "simo@test.it", false);
+loginUser("h", "h@t.it");
---
+++ 02Basics/myfunctions.ts
@@ -0,0 +1,22 @@
+function addTwo(num: number) {
+  
+  return num + 2;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+addTwo(5);
+
+getUpper("simone");
+
+signUpUser("simone","simo@test.it",false);
+
+loginUser("h", "h@t.it")
+
+export {};
