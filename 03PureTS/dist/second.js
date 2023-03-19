a7e5edf1a7c0061a000d6a6a64801fed82ff24fa
Simone Taeggi <startae14@gmail.com>
2023-03-19 17:46:05 +0100

    22_Classes_implements_Interfaces

---
+++ 03PureTS/dist/second.js
@@ -0,0 +1,19 @@
+"use strict";
+class Instragram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class Youtube {
+    constructor(cameraMode, filter, burst, short) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.short = short;
+    }
+    createStory() {
+        console.log("Story was created");
+    }
+}
