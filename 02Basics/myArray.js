5b5d31e058e37b0ea074ac490a047697d79599e3
Simone Taeggi <startae14@gmail.com>
2023-03-19 11:00:55 +0100

    10_array_types

---
+++ 02Basics/myArray.js
@@ -0,0 +1,15 @@
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+// multidimensional arrays
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({
+    name: "",
+    isActive: true
+});
---
+++ 02Basics/myArray.ts
@@ -0,0 +1,26 @@
+const superHeros: string[] = [];
+// const heroPower: number[] = [];
+
+const heroPower: Array<number> = [];
+
+// array of complex data
+type User = {
+  name: string
+  isActive: boolean
+};
+
+const allUsers: User[] = [];
+
+// multidimensional arrays
+const MLModels: number[][] = [
+  [255, 255, 255],
+  []
+];
+
+superHeros.push("spiderman");
+heroPower.push(2);
+
+allUsers.push({
+  name: "",
+  isActive: true
+});
