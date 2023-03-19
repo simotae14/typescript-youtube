aed9b76f92c16284b2108e2e83a0a95386c2ca13
Simone Taeggi <startae14@gmail.com>
2023-03-19 16:01:27 +0100

    15_Classes

--- 03PureTS/dist/index.js
+++ 03PureTS/dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log("typescript is here still");
-console.log("typescript is amazing");
+class User {
+    constructor(email, name) {
+        this.city = "Bergamo";
+        this.email = email;
+        this.name = name;
+    }
+}
+const simone = new User('simone', 's@s.it');
+// simone.city = 'Bergamo';
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,2 +1,12 @@
-console.log("typescript is here still");
-console.log("typescript is amazing");
+class User {
+  email: string;
+  name: string;
+  readonly city: string = "Bergamo";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const simone = new User('simone', 's@s.it');
+// simone.city = 'Bergamo';
