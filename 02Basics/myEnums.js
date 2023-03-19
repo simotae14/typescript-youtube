eab384c4fdcaeafb3b28480c97623b5f76e7fe27
Simone Taeggi <startae14@gmail.com>
2023-03-19 12:44:45 +0100

    12_Enum_types

---
+++ 02Basics/myEnums.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+;
+var stSeat = "AISLE" /* SeatChoice.AISLE */;
---
+++ 02Basics/myEnums.ts
@@ -0,0 +1,10 @@
+const enum SeatChoice {
+  AISLE = "AISLE",
+  MIDDLE = 3,
+  WINDOW,
+  FOURTH
+};
+
+const stSeat = SeatChoice.AISLE
+
+export {};
