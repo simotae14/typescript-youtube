652f2f8d722c14ca043370484d36b96e6b46a778
Simone Taeggi <startae14@gmail.com>
2023-03-20 11:26:47 +0100

    28_In_operator_for_Narrowing

--- 03PureTS/dist/detection.js
+++ 03PureTS/dist/detection.js
@@ -25,3 +25,8 @@ function printAll(strs) {
         }
     }
 }
+function isAdminAccount(account) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+}
--- 03PureTS/src/detection.ts
+++ 03PureTS/src/detection.ts
@@ -25,3 +25,21 @@ function printAll(strs: string | string[] | null) {
     }
   }
 }
+
+// The in operator narrowing
+interface User {
+  name: string,
+  email: string
+}
+
+interface Admin {
+  name: string,
+  email: string,
+  isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+}
