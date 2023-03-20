280b24b474df9ca246bcd618a41002fe58838d9a
Simone Taeggi <startae14@gmail.com>
2023-03-20 12:35:23 +0100

    32_Type_never_and_Exhaustiveness_checking

--- 03PureTS/dist/detection.js
+++ 03PureTS/dist/detection.js
@@ -56,5 +56,18 @@ function getTrueShape(shape) {
     if (shape.kind === "circle") {
         return Math.PI * shape.radius ** 2;
     }
-    return shape.side * shape.side;
+    // return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
 }
--- 03PureTS/src/detection.ts
+++ 03PureTS/src/detection.ts
@@ -95,11 +95,25 @@ interface Rectangle {
   width: number
 }
 
-type Shape = Circle | Square;
+type Shape = Circle | Square | Rectangle;
 
 function getTrueShape(shape: Shape) {
   if (shape.kind === "circle") {
     return Math.PI * shape.radius ** 2;
   }
-  return shape.side * shape.side;
+  // return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+  switch(shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+    default:
+      const _defaultforshape: never = shape;
+      return _defaultforshape;
+  }
 }
